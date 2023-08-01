740558217cee65c62e9b68ef6f0c80d40489ff29
Aimen El mouzammil <ael-mouz@e3r4p16.1337.ma>
2023-08-01 07:10:22 +0100

    v 0.0.3

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,62 +1,108 @@
 #!/bin/bash
 
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+
+print_error() {
+    echo -e "${RED}${BOLD}Error:${RESET} $1"
+}
+
+print_success() {
+    echo -e "${GREEN}${BOLD}Success:${RESET} $1"
+}
+
 install_local_brew() {
+    clear
     if ! command -v brew &>/dev/null; then
-        echo "Homebrew not found. Installing Homebrew..."
-        curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
+        echo -e "${YELLOW}${BOLD}Homebrew not found. Installing Homebrew...${RESET}"
+        if ! curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh; then
+            print_error "Failed to install Homebrew."
+            exit 1
+        fi
     else
-        echo "Homebrew is already installed."
-        echo "Updating Homebrew..."
-        brew update
-        brew upgrade
+        echo -e "${GREEN}${BOLD}Homebrew is already installed.${RESET}"
+        echo -e "${YELLOW}${BOLD}Updating Homebrew...${RESET}"
+        if ! brew update; then
+            print_error "Failed to update Homebrew."
+            exit 1
+        fi
+        if ! brew upgrade; then
+            print_error "Failed to upgrade Homebrew packages."
+            exit 1
+        fi
     fi
 }
 
 install_readline() {
-    echo "Installing readline..."
-    brew install readline
-    brew link readline
+    echo -e "${YELLOW}${BOLD}Installing readline...${RESET}"
+    if ! brew install readline; then
+        print_error "Failed to install readline."
+        exit 1
+    fi
+    if ! brew link readline; then
+        print_error "Failed to link readline."
+        exit 1
+    fi
+    print_success "Readline installed successfully."
 }
 
 install_valgrind() {
-    echo "Installing valgrind..."
-    brew tap LouisBrunner/valgrind
-    brew install --HEAD LouisBrunner/valgrind/valgrind
-    brew upgrade --fetch-HEAD LouisBrunner/valgrind/valgrind
+    echo -e "${YELLOW}${BOLD}Installing valgrind...${RESET}"
+    if ! brew tap LouisBrunner/valgrind; then
+        print_error "Failed to tap LouisBrunner/valgrind."
+        exit 1
+    fi
+    if ! brew install --HEAD LouisBrunner/valgrind/valgrind; then
+        print_error "Failed to install valgrind."
+        exit 1
+    fi
+    if ! brew upgrade --fetch-HEAD LouisBrunner/valgrind/valgrind; then
+        print_error "Failed to upgrade valgrind."
+        exit 1
+    fi
+    print_success "Valgrind installed successfully."
 }
 
 clear
 
-echo "Select an option to install the desired package:"
-echo "0 - brew"
-echo "1 - readline"
-echo "2 - valgrind"
-echo "3 - exit"
+echo -e "${BOLD}Select an option to install the desired package:${RESET}"
+echo -e " ${BOLD}[0]${RESET} - ${GREEN}Homebrew${RESET}"
+echo -e " ${BOLD}[1]${RESET} - ${GREEN}Readline${RESET}"
+echo -e " ${BOLD}[2]${RESET} - ${GREEN}Valgrind${RESET}"
+echo -e " ${BOLD}[3]${RESET} - ${RED}Exit${RESET}"
 
-read -p "Enter your choice: " choice
+read -p  "Enter your choice: " choice
 
 case $choice in
     0)
         install_local_brew
+        echo -e "${GREEN}${BOLD}Setup complete.${RESET}"
         ;;
 
     1)
         install_local_brew
         install_readline
+        echo -e "${GREEN}${BOLD}Setup complete.${RESET}"
         ;;
 
     2)
         install_local_brew
         install_valgrind
+        echo -e "${GREEN}${BOLD}Setup complete.${RESET}"
         ;;
 
     3)
-        echo "Exiting..."
+        echo -e "${RED}${BOLD}Exiting...${RESET}"
         ;;
 
     *)
-        echo "Invalid choice. Exiting..."
+        print_error "Invalid choice. Exiting..."
+        exit 1
         ;;
 esac
 
-echo "Setup complete."
